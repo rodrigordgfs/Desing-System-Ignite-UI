7face8b6fe374f8c06db8b3aa7d51c029693566e
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-04-17 20:11:10 -0300

    Feature: Component - MultiStep

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,25 @@
+import { Label, MultiStepContainer, Step, Steps } from './style'
+
+export interface MultiStepProps {
+  size: number
+  currentStep: number
+}
+
+export function MultiStep({ size, currentStep }: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return (
+            <Step key={step} active={currentStep >= step}>
+              {step}
+            </Step>
+          )
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
